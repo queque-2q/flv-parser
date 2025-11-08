2f0321f00dc9a2dfcdde81a1f9fb8a85726c8bb5
queque-2q <2505184875@qq.com>
2025-11-08 16:48:49 +0800

    修改样式
    * 删除误提交的文件
    
    * 删除多余文件
    
    * 修改action
    
    * 修改样式

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QIcon>
+#include <QStyleFactory>
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/app.ico"));
+    a.setStyle(QStyleFactory::create("Fusion"));
+
     qInstallMessageHandler(customMessageHandler);
     MainWindow w;
     w.show();
